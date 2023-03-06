0ecb54d71b96726be0201fa4abb616ca05b8280c
Jorge Ochoa Reyes <jorge@bytekitchen.io>
2023-03-05 21:24:13 -0800

    removed comments in tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,19 +5,14 @@ module.exports = {
     extend: {},
     screens: {
       "sm": "100px",
-      // => @media (min-width: 640px) { ... }
 
       "md": "481px",
-      // => @media (min-width: 768px) { ... }
 
       "lg": "769px",
-      // => @media (min-width: 1024px) { ... }
 
       "xl": "1025px",
-      // => @media (min-width: 1280px) { ... }
 
       "2xl": "1201px",
-      // => @media (min-width: 1536px) { ... }
     }
   },
   plugins: [
