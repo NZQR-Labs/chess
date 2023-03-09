2efe1a74d92cea0ccfe00c8ffd16fb3f95a137c0
eric <eleow@mail.sfsu.edu>
2023-03-09 11:33:30 -0800

    play again alert

---
+++ src/components/Chess/PlayAgainAlert.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+interface Props {
+  open: boolean;
+  winner: string | null;
+  onReset: () => void;
+}
+
+const PlayAgainAlert: React.FC<Props> = ({ open, winner, onReset }) => {
+  return (
+    <div>
+      <input
+        checked={open}
+        type="checkbox"
+        id="play-again-modal-toggle"
+        readOnly
+        className="modal-toggle"
+      />
+
+      <div className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">{`${winner} wins!`}</h3>
+          <p className="py-4">Do you want to play again?</p>
+          <div className="modal-action">
+            <label htmlFor="play-again-modal-toggle" onClick={onReset} className="btn mr-2">
+              Yes
+            </label>
+            <label htmlFor="play-again-modal-toggle" className="btn" onClick={() => onReset()}>
+              No
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PlayAgainAlert;
